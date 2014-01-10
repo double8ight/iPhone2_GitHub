ca71400a8acae3d7833eb905b64c7c2285063500
double8ight <sevendays_@hanmail.net>
2014-01-10 17:44:12 +0900

    2.6.9 (필수과제) 카탈로그에 네비게이션을 넣었습니다.

---
+++ day11_2_6_9_catalogNavigation/CartItem.m
@@ -0,0 +1,16 @@
+//
+//  CartItem.m
+//  day10_1_2_3_CatalogApp
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "CartItem.h"
+
+@implementation CartItem
+{
+    
+}
+
+@end
---
+++ day11_2_6_9_catalogNavigation/day10_1_2_3_CatalogAppTests/day10_1_2_3_CatalogAppTests.m
@@ -0,0 +1,34 @@
+//
+//  day10_1_2_3_CatalogAppTests.m
+//  day10_1_2_3_CatalogAppTests
+//
+//  Created by SDT-1 on 2014. 1. 9..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface day10_1_2_3_CatalogAppTests : XCTestCase
+
+@end
+
+@implementation day10_1_2_3_CatalogAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
